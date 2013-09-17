8d59c7b09e140f661a2f03e5b432b55df86b1d36
Tomas Radej <tomas@radej.cz>
2013-09-17 14:42:28 +0200

    Adjusted for my needs

--- fedora-postinstall.sh
+++ fedora-postinstall.sh
@@ -3,35 +3,16 @@
 # Post-install script - Fedora 19
 
 # RPM Fusion repository
-yum localinstall --nogpgcheck http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y;
+yum localinstall --nogpgcheck \
+    http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+    http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm \
+    -y;
 
-# Audio Video codecs + VLC
-yum install gstreamer-plugins-{good,bad,ugly} gstreamer-ffmpeg ffmpeg vlc gstreamer1-plugins-ugly gstreamer1-libav -y ;
+# User graphics stuff
+yum install gnome-mplayer libreoffice-writer -y
 
-# Libre Office
-yum install libreoffice-{writer,calc,impress} -y;
-
-# Google Chrome stable
-# Enable Google YUM repository
-touch /etc/yum.repos.d/google-chrome.repo
-echo "
-[google-chrome]
-name=google-chrome - 32-bit
-baseurl=http://dl.google.com/linux/chrome/rpm/stable/i386
-enabled=1
-gpgcheck=1
-gpgkey=https://dl-ssl.google.com/linux/linux_signing_key.pub
-
-name=google-chrome - 64-bit
-baseurl=http://dl.google.com/linux/chrome/rpm/stable/x86_64
-enabled=1
-gpgcheck=1
-gpgkey=https://dl-ssl.google.com/linux/linux_signing_key.pub" >> /etc/yum.repos.d/google-chrome.repo
-yum install google-chrome-stable -y
-
-# Graphical stuff - Gimp (photo editor), Hugin (panoramic tool)
-yum install gimp hugin -y
-
-# Gnome Tweak tool to customize advanced GNOME 3 options
-yum gnome-tweak-tool -y
+# Tools
+if [ $1 -eq 'tools' ]; then
+    yum install git qgit meld vim wget file -y
+fi
 
