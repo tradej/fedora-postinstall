ab7894ef53b1571cbee6b5ed0b33b3eb74339a2f
Tomas Radej <tomas@radej.cz>
2013-11-01 09:14:07 +0100

    Fonts

--- fedora-postinstall.sh
+++ fedora-postinstall.sh
@@ -11,6 +11,29 @@ yum localinstall --nogpgcheck \
 # User graphics stuff
 yum install \
     elinks firefox gnome-mplayer htop @libreoffice qpdfview \
-    file git qgit meld vim wget terminator \
+    file git qgit meld vim vim-jedi wget terminator \
     #rpmdevtools mock xchat fedora-release-rawhide pkgdb-cli \
     -y
+
+# Fonts
+pushd ~/.fonts
+
+    # PT Serif
+    mkdir pt-serif
+    pushd pt-serif
+    wget http://www.fontsquirrel.com/fonts/download/pt-serif -O pt-serif.zip
+    unzip pt-serif.zip
+    rm pt-serif.zip
+    popd
+
+    # Aurulent # TODO Aurulent with patched symbols
+    mkdir aurulent
+    pushd aurulent
+    wget http://www.fontsquirrel.com/fonts/download/Aurulent-Sans-Mono -O aurulent.zip
+    unzip aurulent.zip
+    rm aurulent.zip
+    popd
+
+    fc-cache
+
+
